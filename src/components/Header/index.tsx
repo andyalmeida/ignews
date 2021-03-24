3967a03c413c5227da896058c8801d3b31180858
André Almeida <andalvalmeida@gmail.com>
2021-03-24 11:51:08 -0300

    Creating Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import style from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={style.headerContainer}>
+      <div className={style.headerContent}>
+        <img src="/images/logo.svg" alt="" />
+        <nav>
+          <a className={style.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}

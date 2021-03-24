e59245f5e40c04e41ebc450d892c49769aa474b3
André Almeida <andalvalmeida@gmail.com>
2021-03-24 20:43:19 -0300

    Authentication with NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import style from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={style.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      André Almeida
+      {session.user.name}
       <FiX color="#737380" className={style.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={style.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
